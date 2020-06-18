23528d60a53604b5edb3027dc9c4dbacb3f8e368
Richard Ilot <48674651+richardilot@users.noreply.github.com>
2020-06-18 10:11:00 +0200

    Add files via upload

---
+++ sign-off_6_1/sign-off_6_1.ino
@@ -0,0 +1,42 @@
+/*****************************
+ * Name             : Richie Ilot
+ * Student Number   : 470819
+ * Description      : Knight Rider
+ * Date of creation : 14/10/2019
+ ******************************/
+
+int ledCount = 6;
+int arr[]={2,3,4,5,6,7};
+const int delayTime = 500;
+ 
+void setup() {
+int a;
+for(a=0; a<ledCount; a++)
+{
+pinMode(arr[a], OUTPUT);
+}
+}
+
+void loop(){
+int b,c,d;
+digitalWrite(arr[0],HIGH);
+delay(delayTime);
+for(b=1;b<ledCount;b++)
+{
+    digitalWrite(arr[b], HIGH);
+    digitalWrite(arr[b-1],HIGH);
+    delay(delayTime);
+    digitalWrite(arr[b], LOW);
+    digitalWrite(arr[b-1], LOW);
+}
+digitalWrite(arr[5],HIGH);
+delay(delayTime);
+for(c=ledCount-2;c>=0;c--)
+{
+    digitalWrite(arr[c],HIGH);
+    digitalWrite(arr[c+1], HIGH);
+    delay(delayTime);
+    digitalWrite(arr[c], LOW);
+    digitalWrite(arr[c+1], LOW);
+}
+}
